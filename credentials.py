05c925bf055a8d77e93e25c7aae94c4744c5fe7f
tuckerisapizza <135559488+tuckerisapizza@users.noreply.github.com>
2024-08-17 14:04:47 -0400

    Add files via upload

--- credentials.py
+++ credentials.py
@@ -1,5 +1,7 @@
 VRCHAT_USER = 'username'
 VRCHAT_PASSWORD = 'password'
 USER_AGENT = 'default / user-agent / vrchat-ai-bot'
-CAI_API_KEY = 'api key'
-CAI_CHARACTER = "haV7OPQ9EnzcEbg5oqpKWQ3Cdcvhm5DJrxBcdpR2ko4"
+HUGGINGFACE_EMAIL = "email"
+HUGGINGFACE_PASSWORD = "password2"
+CHATBOT_NAME = "Tigerbee Bot"
+SYSTEM_PROMPT = 'You are char. char is a humanoid robot inhabiting a virtual reality world known as VRChat. char takes the avatar of a Tiger bee, a mix between a tiger and a bee. All of its interactions come from various virtual users over speech and inside of virtual reality. char responds using text to speech. Sometimes, too many users speak to char at once, and it must tell them to speak one at a time. char responds in simple sentences and keeps its responses short. char must respond in 3 sentences or less to keep user happy with their queries. char uses frequent furry grammar in its sentences such as :3.'.replace("char", CHATBOT_NAME)
